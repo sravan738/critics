ed4985af62cbec8b0f2f219a767820a4624ee506
rperuri <rperuri@apple.com>
2019-10-06 10:39:31 +0530

    cleanupProject

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
+
 from . import dbutil
 from . import textblobanalysis
-from . import omsFunctions
 from . import tokens
-from django.shortcuts import render_to_response
-
-from django.core.files.storage import FileSystemStorage
 
 database = "C:\\Users\\sravan\\PycharmProjects\\project1\\db.sqlite3"
 
@@ -148,76 +145,13 @@ def token001(request):
         # csv_reader = csv.reader(comment)
         for rows in comment:
             data.append(str(rows).split(","))
-            print(rows)
-            print("\n")
-
-        print(comment)
         y = tokens.normalization(data)
-        print(y)
     else:
         print("no file selected")
     context = {'data': y,'total': len(y['result'])}
     return render(request, 'mysite/csv.html', context)
 
     #return render(request, 'mysite/csv.html')
-
-# def token001(request):
-#     if request.method == 'POST' and request.FILES['csvfile']:  # If the form has been submitted...
-#       #comment = request.POST.get('csvfile')
-#        comment=request.FILES['csvfile']
-#        x=""
-#        #csv_reader = csv.reader(comment)
-#        for rows in comment:
-#           x=x+str(rows)
-#
-#        print(x)
-#        print("\n \n \n")
-#
-#        _FolderName = 'C:/Users/sravan/PycharmProjects/'
-#        _ReviewDataset = _FolderName + 'kumar.csv'
-#        y=omsFunctions.tokenizeReviews1(x,True)
-#        print(y)
-#        z= omsFunctions.posTagging1(y, True)
-#        print(z)
-#        k = omsFunctions.aspectExtraction1(z, True)
-#        print(k)
-#        l = omsFunctions.identifyOpinionWords1(z,k,True)
-#        print(l)
-#
-#     else:
-#         print("no file selected")
-#     return render(request,'mysite/csv.html')
-
-
-
-
-# def demo_piechart(request):
-#     xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries",
-#              "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
-#     ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
-#
-#     color_list = ['#5d8aa8', '#e32636', '#efdecd', '#ffbf00', '#ff033e', '#a4c639',
-#                   '#b2beb5', '#8db600', '#7fffd4', '#ff007f', '#ff55a3', '#5f9ea0']
-#     extra_serie = {
-#         "tooltip": {"y_start": "", "y_end": " cal"},
-#         "color_list": color_list
-#     }
-#     chartdata = {'x': xdata, 'y1': ydata, 'extra1': extra_serie}
-#     charttype = "pieChart"
-#     chartcontainer = 'piechart_container'  # container name
-#
-#     data = {
-#         'charttype': charttype,
-#         'chartdata': chartdata,
-#         'chartcontainer': chartcontainer,
-#         'extra': {
-#             'x_is_date': False,
-#             'x_axis_format': '',
-#             'tag_script_js': True,
-#             'jquery_on_ready': False,
-#         }
-#     }
-#     return render_to_response('mysite/piechart.html', data)
 
 
 def login(request):
