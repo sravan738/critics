9d7d9fba93e21a3f175fe9b68e7c952ce88a750e
rperuri <rperuri@apple.com>
2019-10-08 22:52:07 +0530

    tillPosTag

---
+++ nltk_imports.py
@@ -0,0 +1,7 @@
+# This file holds the nltk packages to be installed as part of whole process
+
+import nltk
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
