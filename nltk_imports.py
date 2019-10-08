af2be01947124320219f4b1fcf5ceba4376f1c57
sravan738 <44638915+sravan738@users.noreply.github.com>
2019-10-08 23:00:05 +0530

    Revert "tillPosTag"

--- nltk_imports.py
+++
@@ -1,7 +0,0 @@
-# This file holds the nltk packages to be installed as part of whole process
-
-import nltk
-
-nltk.download('wordnet')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
