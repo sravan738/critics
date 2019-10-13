0abed442951ee9bdbd9b87d76f07356dce6395d2
rperuri <rperuri@apple.com>
2019-10-13 23:46:04 +0530

    FinalChanges

--- mysite/data_visualization.py
+++ mysite/data_visualization.py
@@ -1,12 +1,13 @@
 import json
 import re
-from mysite.tokens import Utilities
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, SnowballStemmer, WordNetLemmatizer
 from nltk import pos_tag
+from operator import itemgetter
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-class visualize:
+class Visualize:
     def __init__(self, review):
         self.review = review
 
@@ -33,11 +34,14 @@ class visualize:
         # Step 7 Analyser
         result.update({"analyser": self.analyser})
 
+        # step 8 Result
+        result.update({"assertion": self.assertion})
+
         return result
 
     @property
     def tokenization(self):
-        exclude = "?:!.,;.-_`~+=#()/\|][*' "
+        exclude = "%?:!.,;.-_`~+=#()/\|][*' "
         words = self.review.lower().split(" ") if Utilities.isheader(self.review) else [self.review.lower()]
         self.review = [word.strip(exclude) for word in words if word.strip(exclude)]
         return self.review
@@ -46,6 +50,7 @@ class visualize:
     def remove_stop_words(self):
         stop_words = stopwords.words('english')
         stop_words.append('nt')
+        stop_words.remove("not")
         self.review = [word for word in self.review if word not in stop_words]
         return self.review
 
@@ -131,7 +136,7 @@ class visualize:
                 number_of_adjective += 1
             elif re.match('DT', pos):
                 number_of_interjection += 1
-                
+
         pos_tagged_review["number_of_proper_nouns"] = number_of_proper_nouns
         pos_tagged_review["number_of_other_nouns"] = number_of_other_nouns
         pos_tagged_review["number_of_pronouns"] = number_of_pronouns
@@ -150,11 +155,108 @@ class visualize:
 
     @property
     def analyser(self):
+        input_text = " ".join(map(lambda x: x[0], self.review))
+        sia = SentimentIntensityAnalyzer()
+        score_map = sia.polarity_scores(input_text)
+        self.review = {
+            "negative": score_map["neg"],
+            "neutral": score_map["neu"],
+            "positive": score_map["pos"]
+        }
         return self.review
+
+    @property
+    def assertion(self):
+        negative, positive, neutral = itemgetter("negative", "positive", "neutral")(self.review)
+        if positive > negative:
+            return "positive"
+        elif (positive + neutral) >= (negative + neutral):
+            return "positive"
+        elif (positive + neutral) < (negative + neutral):
+            return "negative"
+        else:
+            return "negative"
+
+
+class Utilities:
+    ones = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine"
+    }
+    tens = {
+        1: "ten",
+        2: "twenty",
+        3: "thirty",
+        4: "forty",
+        5: "fifty",
+        6: "sixity",
+        7: "seventy",
+        8: "eighty",
+        9: "ninty"
+    }
+    hundred = "hundred"
+    thousand = "thousand"
+
+    numbers = {
+        0: "ones",
+        1: "tens",
+        2: "hundred",
+        3: "thousand",
+        4: "thousand",
+        5: "lakhs",
+        6: "lakhs"
+    }
+
+    def convert_int_to_text(self, text):
+        # 100 -> one hundred , 456 -> four hundred fifty six
+        # Doesn't display zero
+        # 456 -> list(text) -> [4,5,6] -> [-1::-1] -> [6,5,4] -> [(0,6),(1,5),(2,4)]
+        # 400 -> list(text) -> [4, 0, 0] -> [-1::-1] -> [0, 0, 4] -> [(0, 0), (1, 0), (2, 4)]
+
+        return_text = []
+        for i, num in enumerate(list(text)[::-1]):
+            if not int(num) == 0:
+                if i in [2, 3, 5]:
+                    return_text.insert(0, self.numbers[i])
+                    return_text.insert(0, self.ones.get(int(num)))
+                elif i in [4, 6]:
+                    return_text.insert(0, self.numbers[i])
+                    return_text.insert(0, self.tens.get(int(num)))
+                else:
+                    return_text.insert(0, getattr(self, self.numbers[i]).get(int(num)))
+        return " ".join(return_text)
+
+    @classmethod
+    def negation(cls, input_text):
+        # input text is of form List[String]
+        negation_text = []
+        for word in input_text:
+            if word.endswith("n't"):
+                negation_text.extend([word[:-3], "not"])
+            else:
+                negation_text.append(word)
+        return negation_text
+
+    @staticmethod
+    def isheader(text):
+        try:
+            if text.index(" "):
+                return True
+            else:
+                return False
+        except Exception as e:
+            return False
 
 
 if __name__ == '__main__':
     text = "Once again Mr. Costner has dragged out a movie for far longer than necessary. Aside from the terrific sea rescue sequences, of which there are very few I just did not care about any of the characters. Most of us have ghosts in the closet, and Costner's character are realized early on, and then forgotten until much later, by which time I did not care. The character we should really care about is a very cocky, overconfident Ashton Kutcher. The problem is he comes off as kid who thinks he's better than anyone else around him and shows no signs of a cluttered closet. His only obstacle appears to be winning over Costner. Finally when we are well past the half way point of this stinker, Costner tells us all about Kutcher's ghosts. We are told why Kutcher is driven to be the best with no prior inkling or foreshadowing. No magic here, it was all I could do to keep from turning it off an hour in."
-    visualize_obj = visualize(text)
+    visualize_obj = Visualize(text)
     result = visualize_obj.process()
     print(json.dumps(result))
--- mysite/tokens.py
+++ mysite/tokens.py
@@ -1,92 +1,4 @@
-import csv
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk.stem import PorterStemmer, SnowballStemmer,WordNetLemmatizer
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-
-class Utilities:
-    ones = {
-        1: "one",
-        2: "two",
-        3: "three",
-        4: "four",
-        5: "five",
-        6: "six",
-        7: "seven",
-        8: "eight",
-        9: "nine"
-    }
-    tens = {
-        1: "ten",
-        2: "twenty",
-        3: "thirty",
-        4: "forty",
-        5: "fifty",
-        6: "sixity",
-        7: "seventy",
-        8: "eighty",
-        9: "ninty"
-    }
-    hundred = "hundred"
-    thousand = "thousand"
-
-    numbers = {
-        0: "ones",
-        1: "tens",
-        2: "hundred",
-        3: "thousand",
-        4: "thousand",
-        5: "lakhs",
-        6: "lakhs"
-    }
-
-    def convert_int_to_text(self, text):
-        # 100 -> one hundred , 456 -> four hundred fifty six
-        # Doesn't display zero
-        # 456 -> list(text) -> [4,5,6] -> [-1::-1] -> [6,5,4] -> [(0,6),(1,5),(2,4)]
-        # 400 -> list(text) -> [4, 0, 0] -> [-1::-1] -> [0, 0, 4] -> [(0, 0), (1, 0), (2, 4)]
-
-        return_text = []
-        for i, num in enumerate(list(text)[::-1]):
-            if not int(num) == 0:
-                if i in [2, 3, 5]:
-                    return_text.insert(0, self.numbers[i])
-                    return_text.insert(0, self.ones.get(int(num)))
-                elif i in [4, 6]:
-                    return_text.insert(0, self.numbers[i])
-                    return_text.insert(0, self.tens.get(int(num)))
-                else:
-                    return_text.insert(0, getattr(self, self.numbers[i]).get(int(num)))
-        return " ".join(return_text)
-
-    @classmethod
-    def negation(cls, input_text):
-        # input text is of form List[String]
-        negation_text = []
-        for word in input_text:
-            if word.endswith("n't"):
-                negation_text.extend([word[:-3], "not"])
-            else:
-                negation_text.append(word)
-        return negation_text
-
-    @staticmethod
-    def isheader(text):
-        try:
-            if text.index(" "):
-                return True
-            else:
-                return False
-        except Exception as e:
-            return False
-
-
-class Normalization:
-    def __init__(self, input_data, positive=0, negative=0):
-        self.input_data = input_data
-        self.positive = positive
-        self.negative = negative
+from mysite.data_visualization import Visualize, Utilities
 
 
 def casing_the_characters(input_text):
@@ -103,16 +15,6 @@ def casing_the_characters(input_text):
 
 
 columns_to_take = ["review"]
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 
 def csv_wrapper(func):
@@ -131,63 +33,22 @@ def csv_wrapper(func):
     return csv_inner_function
 
 
-def semantic_analyser(input_text):
-    sia = SentimentIntensityAnalyzer()
-    score_map = sia.polarity_scores(input_text)
-    if score_map["pos"] > score_map["neg"]:
-        return [input_text, "positive"]
-    else:
-        return [input_text, "negative"]
-
-    # d = {"neg": 0.0, "neu": 0.661, "compound": 0.6249, "pos": 0.339}
-    # for semantic, value in score_map.items():
-    #     if value == max(score_map.values()):
-    #          return [input_text, semantic]
-    # return [input_text, "neu"]
-
-
-def word_net_lemmatizer(input_text):
-    wnl = WordNetLemmatizer()
-    word_net_lemmatizer_stemmer_text = [wnl.lemmatize(word) for word in input_text]
-    return word_net_lemmatizer_stemmer_text
-
-
 @csv_wrapper
 def normalization(*args, **kwargs):
     input_data = kwargs["input_data"]
     outer_result = []
     positive, negative = (0, 0)
     for line in input_data:
-        # Tokenization
-        step1 = line.lower().split(" ") if Utilities.isheader(line) else [line.lower()]
-
-
-        # Removing stop words
-        step2 = [text for text in step1 if text not in stop_words]
-
-        # Character casing
-        step3 = casing_the_characters(step2)
-
-        # Negation
-        step4 = Utilities.negation(step3)
-
-        # stemming - Porter-Stemmer
-        step5 = word_net_lemmatizer(step4)
-
-        # POS-Tagging
-
-        # Random-Forest-Algorithm
-
-        # Combine data
-        step5 = " ".join(step4)
-        step6 = semantic_analyser(step5)
-        if step6[1] == "positive":
+        visualize_obj = Visualize(line)
+        response = visualize_obj.process()
+        text_assertion = response["assertion"]
+        if text_assertion == "positive":
             positive = positive + 1
         else:
             negative = negative + 1
 
-        outer_result.append(step6)
-    return (outer_result, positive, negative)
+        outer_result.append([line, text_assertion])
+    return outer_result, positive, negative
 
 
 if __name__ == '__main__':
